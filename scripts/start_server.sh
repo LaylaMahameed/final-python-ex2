7b151b2e3b1dc80a432f61ef7d8a2b09c84196e3
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-05-01 22:09:56 +0300

    update the port

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Pull the latest version of the Docker image
-docker pull laylmah/final-python-ex2:2-a04eadf1e70cf54755c3c6afb04f0a20e11624bc
+docker pull laylmah/final-python-ex2:2
 
 # Run the Docker container
-docker run -d --name my_app_container -p 5000:5000 laylmah/final-python-ex2:2-a04eadf1e70cf54755c3c6afb04f0a20e11624bc
+docker run -d --name my_app_container -p 5000:5000 laylmah/final-python-ex2:2
 
 echo "Application is running."
 
