e06745a49ed7a8b894edc41e307b16d27613f61b
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-05-01 22:01:57 +0300

    update the the file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-docker run -itd --name final-python -p 5000:5000 laylmah/final-python-ex2:2
+set -e
 
+# Pull the latest version of the Docker image
+docker pull laylmah/final-python-ex2:2-a04eadf1e70cf54755c3c6afb04f0a20e11624bc
+
+# Run the Docker container
+docker run -d --name my_app_container -p 80:80 laylmah/final-python-ex2:2-a04eadf1e70cf54755c3c6afb04f0a20e11624bc
+
+echo "Application is running."
 
 
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker stop final-python
-docker rm final-python
+set -e
+
+# Check if the container is running
+if docker ps -q -f name=my_app_container; then
+    echo "Stopping the container..."
+    docker stop my_app_container
+    docker rm my_app_container
+    echo "Container stopped and removed."
+else
+    echo "Container is not running."
+fi
